85030ed6f4171fbddd08ef0190a01c19bc8c51b7
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-06-02 07:57:33 -0700

    simple server.

--- server.js
+++ server.js
@@ -1,74 +1,8 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-
-var CONTACTS_COLLECTION = "contacts";
-
-var app = express();
-app.use(bodyParser.json());
-
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
-// Connect to the database before starting the application server.
-
-// CONTACTS API ROUTES BELOW
-
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
-
-/*  "/api/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
- app.get("/api/contacts", function(req, res) {
-   db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-     if (err) {
-       handleError(res, err.message, "Failed to get contacts.");
-     } else {
-       res.status(200).json(docs);
-     }
-   });
- });
-
- app.post("/api/contacts", function(req, res) {
-   var newContact = req.body;
-
-   if (!req.body.name) {
-     handleError(res, "Invalid user input", "Must provide a name.", 400);
-   }
-
-   db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-     if (err) {
-       handleError(res, err.message, "Failed to create new contact.");
-     } else {
-       res.status(201).json(doc.ops[0]);
-     }
-   });
- });
-
-/*  "/api/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-app.get("/api/contacts/:id", function(req, res) {
-});
-
-app.put("/api/contacts/:id", function(req, res) {
-});
-
-app.delete("/api/contacts/:id", function(req, res) {
-});
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
