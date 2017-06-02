0a541ebf41ee1fff061feba6fe1450a91cec4615
Matthew Harwood <matthhar12@gmail.com>
2017-06-02 01:28:47 -0700

    - added the cover page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdMenuModule, MdButtonModule, MdTabsModule, MdToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MhPicModule } from './mh/pic/mh-pic.module';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdTabsModule,
+    FlexLayoutModule,
+    MdToolbarModule,
+    MhPicModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/mh/pic/mh-pic.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { environment as env } from '../../../environments/environment';
+
+
+// NOTES:
+// https://internetingishard.com/html-and-css/responsive-images/
+@Component({
+  selector: 'mh-pic',
+  templateUrl: './mh-pic.component.html',
+  styleUrls: ['./mh-pic.component.css']
+})
+export class MhPicComponent implements OnInit {
+  public rootSrc: any;
+
+  @Input('srcLink') srcLink = 'https://placebear.com/g/1920/1080';
+  @Input('altText') altText = 'alt text default';
+  @Input('extType') extType = 'jpg';
+  constructor() {
+    this.rootSrc = env.imgSrc;
+  }
+
+  ngOnInit() {
+    console.log(this)
+  }
+}
